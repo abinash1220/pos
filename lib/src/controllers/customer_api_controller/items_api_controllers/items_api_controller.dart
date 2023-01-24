0345d3eb703a2ce74dd107372663b19a8436896e
anasnaji25 <anasnaji4848@gmail.com>
2023-01-25 02:54:17 +0530

    internet check

--- lib/src/controllers/customer_api_controller/items_api_controllers/items_api_controller.dart
+++ lib/src/controllers/customer_api_controller/items_api_controllers/items_api_controller.dart
@@ -6,6 +6,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:dio/dio.dart' as dio;
+import 'package:internet_connection_checker/internet_connection_checker.dart';
 import 'package:pos/src/const/api_cachekey.dart';
 import 'package:pos/src/models/items_api_models/items_list_api_model.dart';
 import 'package:pos/src/services/items_api_services/create_items_api_service.dart';
@@ -17,31 +18,29 @@ import 'package:pos/src/widgets/snackbar_widgets/incorrect.dart';
 import '../../../models/items_api_models/item_pricelist_model.dart';
 import '../../../models/items_api_models/product_list_model.dart';
 
-class CreateItemsApiController extends GetxController{
+class CreateItemsApiController extends GetxController {
   CreateItemsApiServices createItemsApiServices = CreateItemsApiServices();
   //ListItemsApiService listItemsApiService = ListItemsApiService();
   ItemPriceListService itemPriceListService = ItemPriceListService();
 
- //List<ItemData?>? itemdata = [];
+  //List<ItemData?>? itemdata = [];
 
- List<ItemPrice?>? pricelist = [];
+  List<ItemPrice?>? pricelist = [];
 
   RxString item = "null".obs;
 
-  
   itemsCreate({
-       required BuildContext context,
-       required String items,
-       required String description,
-       required String iva,
-       required String caracteristicas,
-       required String codBarras,
-       required String unidadeBase,
-       required String peso,
-       required String volume,
-       required String marca,
+    required BuildContext context,
+    required String items,
+    required String description,
+    required String iva,
+    required String caracteristicas,
+    required String codBarras,
+    required String unidadeBase,
+    required String peso,
+    required String volume,
+    required String marca,
   }) async {
-   
     dio.Response<dynamic> response = await createItemsApiServices.createItems(
       items: items,
       description: description,
@@ -52,17 +51,15 @@ class CreateItemsApiController extends GetxController{
       peso: peso,
       volume: volume,
       marca: marca,
-      );
-       print(":::::::::::::::::Create items Status ::::::::::::::::::");
+    );
+    print(":::::::::::::::::Create items Status ::::::::::::::::::");
     print(response.statusCode);
-    if(response.statusCode == 204){
-
-        Get.offAll(HomePageWithNavigation(
-          index: 0,
-        ));
-
-    }else {
-         ScaffoldMessenger.of(context).showSnackBar(incorrect);
+    if (response.statusCode == 204) {
+      Get.offAll(HomePageWithNavigation(
+        index: 0,
+      ));
+    } else {
+      ScaffoldMessenger.of(context).showSnackBar(incorrect);
     }
   }
 
@@ -83,47 +80,37 @@ class CreateItemsApiController extends GetxController{
   //      }
   // }
 
- Future<List<ItemPrice?>?> listOfitems({
-    required String client,
-    required String wareHouse
-  }) async {
-        
-        var isChacheExist = await APICacheManager().isAPICacheKeyExist(pricelistKey);
+  Future<List<ItemPrice?>?> listOfitems(
+      {required String client, required String wareHouse}) async {
+    // var isChacheExist = await APICacheManager().isAPICacheKeyExist(pricelistKey);
+    bool result = await InternetConnectionChecker().hasConnection;
 
-        if(!isChacheExist){
+    if (result) {
+      dio.Response<dynamic> response = await itemPriceListService.itempricelist(
+          client: client, wareHouse: wareHouse);
 
-           dio.Response<dynamic> response = await itemPriceListService.itempricelist(client: client,wareHouse: wareHouse);
-         
-         if(response.statusCode == 200){
-
-          APICacheDBModel cacheDBModel = new APICacheDBModel(
-          key: customerlistkey, 
-          syncData: jsonEncode(response.data));
+      if (response.statusCode == 200) {
+        APICacheDBModel cacheDBModel = new APICacheDBModel(
+            key: customerlistkey, syncData: jsonEncode(response.data));
 
         await APICacheManager().addCacheData(cacheDBModel);
 
-          PriceList itempricelists = PriceList.fromJson(response.data);
-          pricelist = itempricelists.data;
+        PriceList itempricelists = PriceList.fromJson(response.data);
+        pricelist = itempricelists.data;
 
-          print("::::::::::::::::printing price list::::::::::::::");
-          print(response.statusCode);
-          
-      
+        print("::::::::::::::::printing price list::::::::::::::");
+        print(response.statusCode);
+
         return pricelist;
-         }
-  }else{
+      }
+    } else {
+      var cacheData = await APICacheManager().getCacheData(pricelistKey);
 
-    var cacheData = await APICacheManager().getCacheData(pricelistKey);
+      PriceList itempricelists =
+          PriceList.fromJson(json.decode(cacheData.syncData));
+      pricelist = itempricelists.data;
 
-       PriceList itempricelists = PriceList.fromJson(json.decode(cacheData.syncData));
-       pricelist = itempricelists.data;
-      
-       update();
-
+      update();
+    }
   }
-
-        }
-        
-       
-
 }
