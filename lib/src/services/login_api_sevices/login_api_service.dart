b7e7d7928bf210bf9744bef4a1b5f39a190b977c
anasnaji25 <anasnaji4848@gmail.com>
2023-02-25 11:18:18 +0530

    changes

--- lib/src/services/login_api_sevices/login_api_service.dart
+++ lib/src/services/login_api_sevices/login_api_service.dart
@@ -1,49 +1,48 @@
 import 'package:dio/dio.dart';
+import 'package:pos/src/const/auth_tokens.dart';
 import 'package:pos/src/services/base_url.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
-class LoginApiServices extends BaseApiService{
-
-   Future loginApiServices ({
+class LoginApiServices extends BaseApiService {
+  Future loginApiServices({
     required String username,
     required String password,
-   }) async{
-      
-      dynamic responseJson;
+  }) async {
+    dynamic responseJson;
 
-     try{
-      
+    try {
       var dio = Dio();
-       
-         FormData formData = FormData.fromMap({
-          "username": username,
-          "password": password,
-          "company":"XXX", 
-          "instance":"DEFAULT",
-          "grant_type":"password",
-          "line":"Executive",
-         });
- 
-         var response = await dio.post(loginUrl,
-           options: Options(
-            headers: {
-              'Accept': 'Application/x-www-form-urlencoded',
-              'Content-Type': 'application/x-www-form-urlencoded',
-            },
-            followRedirects: false,
-                validateStatus: (status) {
-                 return status! <= 500;
-                }),
-            data: {
-          "username": username.trim(),
-          "password": password.trim(),
-          "company":"XXX", 
-          "instance":"DEFAULT",
-          "grant_type":"password",
-          "line":"Executive",
-         });
-            print("::::::::::::::::login Authorization status code::::::::::::::");
-            print(response.statusCode);
+
+      FormData formData = FormData.fromMap({
+        "username": username.trim(),
+        "password": password.trim(),
+        "company": "XXX",
+        "instance": "DEFAULT",
+        "grant_type": "password",
+        "line": "Executive",
+      });
+
+      var response = await dio.post(loginUrl,
+          options: Options(
+              headers: {
+                'Content-Type': 'application/x-www-form-urlencoded',
+                'Authorization': 'Bearer $secondToken'
+              },
+              followRedirects: false,
+              validateStatus: (status) {
+                return status! <= 500;
+              }),
+          data: {
+            "username": username.trim(),
+            "password": password.trim(),
+            "company": "XXX",
+            "instance": "DEFAULT",
+            "grant_type": "password",
+            "line": "Executive",
+          });
+      print("::::::::::::::::login Authorization status code::::::::::::::");
+      print(response.statusCode);
+      print(response.data);
 
       responseJson = response;
     } catch (e) {
@@ -51,6 +50,5 @@ class LoginApiServices extends BaseApiService{
     }
 
     return responseJson;
-   }
-
+  }
 }
